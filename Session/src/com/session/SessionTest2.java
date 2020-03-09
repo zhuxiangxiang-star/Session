1deb01c4734236adc5de8ab751ec559b331f0757
Administrator <Administrator@SD-20190410SPWV>
2020-03-09 10:53:21 +0800

    Commit2

--- Session/src/com/session/SessionTest2.java
+++ Session/src/com/session/SessionTest2.java
@@ -1,7 +1,7 @@
 package com.session;
 
 public class SessionTest2{
-  public static void main(String args[]){
+  public static void main(String args []){
     System.out.print("SessionTest2");
   }
 }
